86119af5907ab27f813f3779db35f00389d0b8c1
bull313 <blladi313@gmail.com>
2018-08-23 22:38:21 -0700

    Linked the /Bids route to the PledgeClass react file (to be changed to the Deliberation react file once that is created).

--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,6 @@ export default (
     <Route path="/" component={App}>
         <IndexRoute component={homePage}/>
         <Route path="/pledgeClasses" component={PledgeClasses}/>
+        <Route path="/Bids" component={PledgeClasses}/>
     </Route>
 );
