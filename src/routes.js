3b015de310b2bb9de8843915f9bb14ecf61fb2a1
bull313 <=>
2018-08-27 11:27:46 -0700

    Created a next button (with Brandon's help)

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,13 @@ import {Route, IndexRoute} from 'react-router';
 import App from './App';
 import homePage from './components/HomePage';
 import PledgeClasses from './components/PledgeClasses';
+import Deliberation from './components/Deliberation';
 
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={homePage}/>
         <Route path="/pledgeClasses" component={PledgeClasses}/>
-        <Route path="/Bids" component={PledgeClasses}/>
+        <Route path="/Bids" component={Deliberation}/>
     </Route>
 );
