0759537d3400671454b10ebe72667af54f47dfe9
bull313 <=>
2018-08-27 11:10:15 -0700

    Updated database

---
+++ tools/db.js
@@ -0,0 +1,59 @@
+const database = {
+
+  /* Pledge Class Table */
+  pledgeClasses: [
+    {
+      className: "Epsilon Gamma",
+      marshal: "Monica Mekhil",
+      pledgeGroup: [
+        "Mohit Doshi",
+        "Justin Fields",
+        "Jacob Funk",
+        "Jackson Holly",
+        "Karolena Lein",
+        "Sam Payne",
+        "Dylan Renner"
+      ]
+    },
+
+    {
+      className: "Zeta Gamma",
+      marshal: "Emily Dawson",
+      pledgeGroup: [
+        "Javier Barrantes",
+        "Eli Carlton",
+        "Benjamin Ladick",
+        "Stephanie Eggen",
+        "Brent Ottley",
+        "Brandon Pacheco",
+        "Angel Valero",
+        "Cole Green",
+        "Kevin Johnson",
+        "Jose Marquez"
+      ]
+    },
+
+    {
+      className: "Eta Gamma",
+      marshal: "Jacob Funk",
+      pledgeGroup: [
+        "Reid Baker",
+        "Allyson Deimeke",
+        "Diego Escalante",
+        "Alex Harken",
+        "Diana Hernandez",
+        "Areli Isherwood",
+        "Tristan Le",
+        "Chris Lee",
+        "Daniel Loza",
+        "Neel Patel",
+        "Chandler Rosas"
+      ]
+    }
+
+  ]
+
+};
+
+/* Export database object */
+module.exports = { database: database };
---
+++ tools/routes.js
@@ -0,0 +1,6 @@
+/* Routes for api fetch calls */
+module.exports = {
+  routes: {
+    PLEDGE_CLASS_ROUTE: "/api/getPledgeClasses"
+  }
+};
